cb02a38021af0ddfbbd858ef2f448f2f3266cd12
lisambamberg <lisambamberg@gmail.com>
2020-09-24 17:42:41 -0500

    Created function to highlight key strokes

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello')
+$(function () {
+    const lowerKeyboard = $("#keyboard-lower-container");
+    const upperKeyboard = $("#keyboard-upper-container")
+    // lowerKeyboard.show();
+    upperKeyboard.hide();
+
+    //lowerKeyboard.toggle();
+    $(document).keydown(function (e) {
+        if (e.keyCode == 16) {
+            lowerKeyboard.hide()
+            upperKeyboard.show()
+        } else {
+            $(document).keyup(function (e) {
+                lowerKeyboard.show()
+                upperKeyboard.hide()
+            })
+        }
+    })
+
+    $(document).keypress(function (e) {
+        $(`#${e.keyCode}`).css("backgroundColor", "yellow");
+        setTimeout(function () {
+            $(`#${e.keyCode}`).css("backgroundColor", "#f5f5f5");
+        }, 300);
+    })
+})
