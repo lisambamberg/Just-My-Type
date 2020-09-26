164235de2c5b93b6d7234836de0a6ed1f64d1e47
lisambamberg <lisambamberg@gmail.com>
2020-09-25 19:42:55 -0500

    Added counter to interate for next  sentence to appear

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 $(function () {
-    //let sentences = ["ten ate neite ate nee enet ite ate inet ent eate", "Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain nate eate tea anne inant nean", "itant eate anot eat nato inate eat anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate"];
-    let sentences = ['ten ate', 'neite ate', 'nee enet'];
-    let sentenceDiv = $("#sentence")
+    let sentences = ["ten ate neite ate nee enet ite ate inet ent eate", "Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain nate eate tea anne inant nean", "itant eate anot eat nato inate eat anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate"];
+    //let sentences = ['ten ate', 'neite ate', 'nee enet'];
+    let sentenceDiv = $("#sentence");
     let yellowBlock = $("#yellow-block");
-    let sentenceCounter = 0;
-    let letterCounter = 0;
-    let currentSentence = sentences[sentenceCounter];
-    let currentLetter = currentSentence[letterCounter];
+    let i = 0;
+    let j = 0;
+    let currentSentence = sentences[i];
+    //let currentLetter = currentSentence[j];
 
-    sentenceDiv.append(sentences[sentenceCounter]);
+    sentenceDiv.html(sentences[i]);
+    $("#target-letter").append(currentSentence[j]);
 
     const lowerKeyboard = $("#keyboard-lower-container");
     const upperKeyboard = $("#keyboard-upper-container")
@@ -17,12 +18,20 @@ $(function () {
 
     $(document).keydown(function (e) {
         let key = e.key.charCodeAt(0);
-        console.log(e.key);
         if (e.keyCode == 16 && e.key === "Shift") {
             lowerKeyboard.hide()
             upperKeyboard.show()
         } else {
             $(`#${key}`).css("backgroundColor", "yellow");
+            console.log(j);
+            console.log("sentences length", sentences[i].length);
+            j++;
+
+            if (j == sentences[i].length) {
+                j = 0;
+                i++;
+                sentenceDiv.html(sentences[i]);
+            }
         }
     })
 
