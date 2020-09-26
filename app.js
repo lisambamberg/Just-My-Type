a5ac18d6932646ee1496825a02b5a82912313d46
lisambamberg <lisambamberg@gmail.com>
2020-09-25 19:08:44 -0500

    Refactored project

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 $(function () {
+    //let sentences = ["ten ate neite ate nee enet ite ate inet ent eate", "Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain nate eate tea anne inant nean", "itant eate anot eat nato inate eat anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate"];
+    let sentences = ['ten ate', 'neite ate', 'nee enet'];
+    let sentenceDiv = $("#sentence")
+    let yellowBlock = $("#yellow-block");
+    let sentenceCounter = 0;
+    let letterCounter = 0;
+    let currentSentence = sentences[sentenceCounter];
+    let currentLetter = currentSentence[letterCounter];
+
+    sentenceDiv.append(sentences[sentenceCounter]);
+
     const lowerKeyboard = $("#keyboard-lower-container");
     const upperKeyboard = $("#keyboard-upper-container")
-    // lowerKeyboard.show();
+
     upperKeyboard.hide();
 
-    //lowerKeyboard.toggle();
     $(document).keydown(function (e) {
-        if (e.keyCode == 16) {
+        let key = e.key.charCodeAt(0);
+        console.log(e.key);
+        if (e.keyCode == 16 && e.key === "Shift") {
             lowerKeyboard.hide()
             upperKeyboard.show()
         } else {
-            $(document).keyup(function (e) {
-                lowerKeyboard.show()
-                upperKeyboard.hide()
-            })
+            $(`#${key}`).css("backgroundColor", "yellow");
         }
     })
 
-    $(document).keypress(function (e) {
-        $(`#${e.keyCode}`).css("backgroundColor", "yellow");
-        setTimeout(function () {
-            $(`#${e.keyCode}`).css("backgroundColor", "#f5f5f5");
-        }, 300);
+    $(document).keyup(function (e) {
+        let key = e.key.charCodeAt(0);
+        lowerKeyboard.show()
+        upperKeyboard.hide()
+        $(`#${key}`).css("backgroundColor", "#f5f5f5");
     })
 })
