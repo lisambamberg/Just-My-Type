01bd9bc58be27c45510e49eb197be6699a839ce7
lisambamberg <lisambamberg@gmail.com>
2020-09-28 13:59:10 -0500

    Changed sentences from short to long

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(function () {
-    //let sentences = ["ten ate neite ate nee enet ite ate inet ent eate", "Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain nate eate tea anne inant nean", "itant eate anot eat nato inate eat anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate"];
-    let sentences = ['ten ate', 'neite ate',];
+    let sentences = ["ten ate neite ate nee enet ite ate inet ent eate", "Too ato too nOt enot one totA not anot tOO aNot", "oat itain oat tain nate eate tea anne inant nean", "itant eate anot eat nato inate eat anot tain eat", "nee ene ate ite tent tiet ent ine ene ete ene ate"];
+    //let sentences = ['ten ate', 'neite ate',];
     let sentenceDiv = $("#sentence");
     let yellowBlock = $("#yellow-block");
     let targetLetter = $("#target-letter");
